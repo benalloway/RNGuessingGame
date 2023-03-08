d380661c709ef3010eb6dc7225ad77c4f83642db
Benjamin Alloway <Benjamin_Alloway@carmax.com>
2023-03-08 09:20:09 -0500

    screens added, custom button

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,29 @@
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput placeholder="Pick a number" />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    borderRadius: 8,
+    backgroundColor: "#72063c",
+
+    elevation: 4, // android drop shadow
+
+    // justifyContent: "center",
+    // alignItems: "center",
+  },
+});
+
+export default StartGameScreen;
