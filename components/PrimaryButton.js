bd934748d4d1a8df897c6eb65727b6bcc4e67ef0
Benjamin Alloway <Benjamin_Alloway@carmax.com>
2023-03-08 09:51:49 -0500

    button styles, screen layout

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,49 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 // use <PrimaryButton>[TEXT]</PrimaryButton>
 const PrimaryButton = ({ children }) => {
+  const onPress = () => console.log("PRESSED: ", children);
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.pressed, styles.buttonInnerContainer]
+            : styles.buttonInnerContainer
+        }
+        onPress={onPress}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+
+    elevation: 2, // android shadow
+    // IOS Shadow:
+    shadowColor: "black", // ios shadow
+    shadowOffset: { width: 0, height: 2 }, // ios shadow
+    shadowRadius: 6, // ios shadow
+    shadowOpacity: 0.25, // ios shadow
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
 
 export default PrimaryButton;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,25 +4,59 @@ import PrimaryButton from "../components/PrimaryButton";
 const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput placeholder="Pick a number" />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <TextInput
+        maxLength={2}
+        inputMode="numeric"
+        keyboardAppearance="light"
+        style={styles.numberInput}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   inputContainer: {
+    alignItems: "center",
+    justifyContent: "center",
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
     borderRadius: 8,
-    backgroundColor: "#72063c",
-
+    backgroundColor: "#4e0329",
     elevation: 4, // android drop shadow
+    // IOS Shadow:
+    shadowColor: "black", // ios shadow
+    shadowOffset: { width: 0, height: 2 }, // ios shadow
+    shadowRadius: 6, // ios shadow
+    shadowOpacity: 0.25, // ios shadow
+  },
+  numberInput: {
+    marginVertical: 8,
+    height: 50,
+    width: 50,
 
-    // justifyContent: "center",
-    // alignItems: "center",
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+
+    textAlign: "center",
+    fontSize: 32,
+    color: "#ddb52f",
+    fontWeight: "bold",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
 
